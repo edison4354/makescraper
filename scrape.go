bf5664b34c3096b0189dce635282303a958f70fb
Edison Li <edison4354@gmail.com>
2022-03-04 12:41:28 -0800

    Basic scraping for current top 5 valorant teams in US

--- scrape.go
+++ scrape.go
@@ -2,29 +2,46 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
 
-// main() contains code adapted from example found in Colly's docs:
-// http://go-colly.org/docs/examples/basic/
+type valTeam struct {
+	Name string
+	Rank int
+}
+
 func main() {
+	var valRanking = []valTeam{}
+
+	var allTeams []string
+
 	// Instantiate default collector
 	c := colly.NewCollector()
-
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
-
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-	})
 
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Something went wrong:", err)
+	})
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("a.rank-item-team.fc-flex", func(e *colly.HTMLElement) {
+		teams := strings.TrimSpace(e.Attr("data-sort-value"))
+		allTeams = append(allTeams, teams)
+	})
+
 	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.Visit("https://www.vlr.gg/rankings/north-america")
+
+	for i := 0; i < 5; i++ {
+		team := valTeam{Name: allTeams[i], Rank: i + 1}
+		valRanking = append(valRanking, team)
+	}
+
+	fmt.Print(valRanking)
 }
